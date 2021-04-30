529f74af9ba4727f73234609c12d71bbf533e197
Olivier Lando <olivier@api.video>
2021-04-30 16:19:46 +0200

    [C#] Several fixes before release

---
+++ templates/csharp/statics/tests/Integration/WebhooksTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApiVideo.Client;
+using ApiVideo.Model;
+
+namespace VideoApiTests.Integration
+{
+    [TestClass]
+    public class WebhooksTest
+    {
+        private ApiVideoClient apiClient;
+        private Webhook webhook;
+
+        [TestInitialize]
+        public void init()
+        {
+            apiClient = new ApiVideoClient(System.Environment.GetEnvironmentVariable("API_KEY"));
+
+        }
+
+        [TestMethod]
+        public void createAndListWebhook()
+        {
+            webhook = apiClient.Webhooks()
+                    .create(new WebhooksCreationPayload() { url = "https://webhooks.test-java-api-client", events = new List<string> { "video.encoding.quality.completed", } });
+            
+
+            Page<Webhook> list = apiClient.Webhooks().list().execute();
+
+            //TODO
+            this.apiClient.Webhooks().delete(this.webhook.webhookid);
+        }
+
+
+    }
+}
