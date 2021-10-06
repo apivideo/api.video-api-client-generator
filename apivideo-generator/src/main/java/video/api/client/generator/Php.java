1e8020e5c078bae48a7834851dfdebc26face6fb
Olivier Lando <olivier@api.video>
2021-10-06 10:07:54 +0200

    Add a first model test

--- apivideo-generator/src/main/java/video/api/client/generator/Php.java
+++ apivideo-generator/src/main/java/video/api/client/generator/Php.java
@@ -220,6 +220,7 @@ public class Php extends AbstractPhpCodegen {
         supportingFiles.add(new SupportingFile("tests/Api/UploadTokensApiTest.php", "", "tests/Api/UploadTokensApiTest.php"));
         supportingFiles.add(new SupportingFile("tests/Api/VideosApiTest.php", "", "tests/Api/VideosApiTest.php"));
         supportingFiles.add(new SupportingFile("tests/Api/WebhooksApiTest.php", "", "tests/Api/WebhooksApiTest.php"));
+        supportingFiles.add(new SupportingFile("tests/Model/VideoTest.php", "", "tests/Model/VideoTest.php"));
         supportingFiles.add(new SupportingFile("phpunit.xml.dist", "", "phpunit.xml.dist"));
 
         // Tests resources
