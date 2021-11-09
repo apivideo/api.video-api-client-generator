5574532941cbd05317d8cd2cfe737e3d444bfa63
Olivier Lando <olivier@api.video>
2021-11-09 18:51:55 +0100

    fix(python) Add missing video_id file

--- templates/python/post-generate.sh
+++ templates/python/post-generate.sh
@@ -1,5 +1,6 @@
 cp -R ../../templates/python/statics/.github ./
 cp -R ../../templates/python/statics/test/* ./test/
+cp -R ../../templates/python/statics/apivideo/model/* ./apivideo/model/
 cp ../../templates/common-resources/test-assets/* test/
 cp ../../templates/common-resources/CoC.md ./
 rm post-generate.sh
