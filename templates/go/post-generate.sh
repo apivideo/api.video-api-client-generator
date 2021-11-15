73d3d67f1a33009f3d999947e49c2ce5bc01420f
Olivier Lando <olivier@api.video>
2021-11-15 11:33:41 +0100

    chore: add changelogs

--- templates/go/post-generate.sh
+++ templates/go/post-generate.sh
@@ -3,5 +3,6 @@ cp -R ../../templates/go/statics/* ./
 mkdir test-assets
 cp ../../templates/common-resources/test-assets/* test-assets
 cp ../../templates/common-resources/CoC.md ./
+cp ../../templates/common-resources/LICENSE ./
 mv test-assets/10m.mp4.part.c test-assets/10m.mp4.part.d
 gofmt -s -w *.go
--- templates/python/post-generate.sh
+++ templates/python/post-generate.sh
@@ -1,6 +1,8 @@
 cp -R ../../templates/python/statics/.github ./
+cp -R ../../templates/python/statics/CHANGELOG.md ./CHANGELOG.md
 cp -R ../../templates/python/statics/test/* ./test/
 cp -R ../../templates/python/statics/apivideo/model/* ./apivideo/model/
 cp ../../templates/common-resources/test-assets/* test/
 cp ../../templates/common-resources/CoC.md ./
+cp ../../templates/common-resources/LICENSE ./
 rm post-generate.sh
