eef62575fb596b19535a5eb95db941ca1856d698
Olivier Lando <olivier@api.video>
2022-03-22 12:32:48 +0100

    Add issue templates

--- templates/java/post-generate-android-uploader.sh
+++ templates/java/post-generate-android-uploader.sh
@@ -3,4 +3,5 @@ cp -R ../../templates/java/statics/android-uploader/* ./
 cp ../../templates/common-resources/test-assets/* src/androidTest/assets/
 cp ../../templates/common-resources/CONTRIBUTING.md ./
 cp ../../templates/common-resources/LICENSE ./
+cp ../../templates/common-resources/.github/ISSUE_TEMPLATE/feature_request.md .github/ISSUE_TEMPLATE/
 mvn package -D skipTests
--- templates/python/post-generate.sh
+++ templates/python/post-generate.sh
@@ -4,4 +4,5 @@ cp -R ../../templates/python/statics/apivideo/model/* ./apivideo/model/
 cp ../../templates/common-resources/test-assets/* test/
 cp ../../templates/common-resources/CONTRIBUTING.md ./
 cp ../../templates/common-resources/LICENSE ./
+cp ../../templates/common-resources/.github/ISSUE_TEMPLATE/feature_request.md .github/ISSUE_TEMPLATE/
 rm post-generate.sh
