598447979f1455444664da6d9e318bf4f2edf823
ThibaultBee <thibault@api.video>
2022-07-01 16:08:02 +0200

    feat(android): add origin sdk field support

--- templates/java/statics/java/src/test/java/video/api/integration/AbstractTest.java
+++ templates/java/statics/java/src/test/java/video/api/integration/AbstractTest.java
@@ -11,6 +11,6 @@ public abstract class AbstractTest {
     AbstractTest() {
         String basePath = Optional.ofNullable(System.getenv().get("INTEGRATION_TESTS_BASE_PATH")).orElse("https://ws.api.video");
         this.apiClient = new ApiVideoClient(new ApiClient(System.getenv().get("INTEGRATION_TESTS_API_KEY"), basePath));
-        this.apiClient.setApplicationName("client-integration-tests");
+        this.apiClient.setApplicationName("client-integration-tests", "0");
     }
 }
