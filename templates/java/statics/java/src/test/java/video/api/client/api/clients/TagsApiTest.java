ae8be84a3c16d51cb2c54de832c763d248d481a8
Olivier Lando <olivier@api.video>
2024-09-26 12:05:42 +0200

    Add video tags endpoint

---
+++ templates/java/statics/java/src/test/java/video/api/client/api/clients/TagsApiTest.java
@@ -0,0 +1,9 @@
+package video.api.client.api.clients;
+
+import org.junit.jupiter.api.DisplayName;
+/**
+ * API tests for TagsApi
+ */
+@DisplayName("TagsApi")
+public class TagsApiTest extends AbstractApiTest {
+}
